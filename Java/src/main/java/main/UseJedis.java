424264fe6e09cf408d5b45477d47443d41b35f63
sulvto <lpetrel@outlook.com>
2016-03-09 14:03:58 +0800

    add cglib

--- Java/src/main/java/main/UseJedis.java
+++ Java/src/main/java/main/UseJedis.java
@@ -15,14 +15,14 @@ public class UseJedis {
         //连接本地的 Redis 服务
         Jedis jedis = new Jedis("localhost");
 
-        System.out.println("Connection to server sucessfully");
         //查看服务是否运行
-        System.out.println("Server is running: " + jedis.ping());
+        LOGGER.debug("Server is running: " + jedis.ping());
+        LOGGER.debug("Connection to server sucessfully");
 
         //设置 redis 字符串数据
         jedis.set("key", "hello world");
         // 获取存储的数据并输出
-        System.out.println("Stored string in redis:: " + jedis.get("key"));
+        LOGGER.info("Stored string in redis:: " + jedis.get("key"));
 
     }
 
