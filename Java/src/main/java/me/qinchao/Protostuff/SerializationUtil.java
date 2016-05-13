b17afc6f69fb3173d874728b80935d756885dd47
sulvto <lpetrel@outlook.com>
2016-05-13 23:56:38 +0800

    ArrayBlockingQueue

--- Java/src/main/java/me/qinchao/Protostuff/SerializationUtil.java
+++ Java/src/main/java/me/qinchao/Protostuff/SerializationUtil.java
@@ -1,7 +1,44 @@
 package me.qinchao.Protostuff;
 
+import io.protostuff.LinkedBuffer;
+import io.protostuff.ProtostuffIOUtil;
+import io.protostuff.Schema;
+import io.protostuff.runtime.RuntimeSchema;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Created by sulvto on 16-5-12.
  */
 public class SerializationUtil {
+    private final static Map<Class, Schema> schemaMap = new ConcurrentHashMap<>();
+
+    public static <T> byte[] serialize(T t) {
+        LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
+        Schema schema = getSchema(t.getClass());
+        try {
+            return ProtostuffIOUtil.toByteArray(t, schema, buffer);
+        } finally {
+            buffer.clear();
+        }
+    }
+
+    public static <T> T deserialize(byte[] bytes, Class<T> aClass) {
+        Schema<T> schema = getSchema(aClass);
+        T message = schema.newMessage();
+        ProtostuffIOUtil.mergeFrom(bytes, message, schema);
+        return message;
+    }
+
+
+    private static <T> Schema<T> getSchema(Class<T> typeClass) {
+        if (schemaMap.containsKey(typeClass)) {
+            return schemaMap.get(typeClass);
+        } else {
+            RuntimeSchema<T> from = RuntimeSchema.createFrom(typeClass);
+            schemaMap.put(typeClass, from);
+            return from;
+        }
+    }
 }
--- Java/src/test/java/me/qinchao/Protostuff/SerializationUtilTest.java
+++ Java/src/test/java/me/qinchao/Protostuff/SerializationUtilTest.java
@@ -1,5 +1,6 @@
 package me.qinchao.Protostuff;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,13 +10,55 @@ import static org.junit.Assert.*;
  */
 public class SerializationUtilTest {
 
-    @Test
-    public void testSerialize() throws Exception {
+    class User {
 
+        private int age;
+        private String name;
+
+        public User() {
+        }
+
+        public User(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "age=" + age +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
     }
 
     @Test
-    public void testDeserialize() throws Exception {
+    public void test() throws Exception {
 
+        byte[] userbyte = SerializationUtil.serialize(new User(20, "username"));
+        System.out.println(userbyte);
+
+        User deserialize = SerializationUtil.deserialize(userbyte, User.class);
+        System.out.println(deserialize);
+
+        Assert.assertEquals(deserialize.getAge(), 20);
+        Assert.assertEquals(deserialize.getName(), "username");
     }
+
 }
