9416d67b40450a12473bc04f664f523249741637
sulvto <lpetrel@outlook.com>
2016-07-01 00:30:44 +0800

    pss templates

---
+++ Java/pss/templates/ServiceImpl.java
@@ -0,0 +1,9 @@
+package me.qinchao.pss.service.impl;
+
+import me.qinchao.pss.domain.${entityDomain};
+import me.qinchao.pss.service.I${entityDomain}Service;
+
+public class ${entityDomain}ServiceImpl extends BaseServiceImpl<${entityDomain}> implements
+		I${entityDomain}Service {
+
+}
---
+++ Java/pss/templates/ServiceTest.java
@@ -0,0 +1,16 @@
+package me.qinchao.pss.service;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import me.qinchao.pss.domain.${entityDomain};
+
+public class ${entityDomain}ServiceTest extends BaseServiceTest<${entityDomain}> {
+	@Autowired
+	private I${entityDomain}Service ${lowerEntityDomain}Service;
+
+	@Test
+	public void test() throws Exception {
+		
+	}
+}
