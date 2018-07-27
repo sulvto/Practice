48b1de70736142b7f537ebb43941c1fa3e389f27
sulvto <sulvto@gmail.com>
2018-07-27 18:31:05 +0800

    myweb

---
+++ k8s/start-kube.sh
@@ -0,0 +1,7 @@
+systemctl start etcd
+systemctl start docker
+systemctl start kube-apiserver
+systemctl start kube-controller-manager
+systemctl start kube-scheduler
+systemctl start kubelet
+systemctl start kube-proxy
