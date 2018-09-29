8c0757adc8e51f3f8ff99afe47b1427822a62841
sulvto <sulvto@gmail.com>
2018-09-30 00:03:31 +0800

    Remove Golang, Java

--- Java/src/main/java/me/qinchao/HelloService/IHelloService.java
+++
@@ -1,8 +0,0 @@
-package me.qinchao.HelloService;
-
-/**
- * Created by SULVTO on 16-3-11.
- */
-public interface IHelloService {
-    public String sayHello(String name);
-}
--- Java/src/main/java/me/qinchao/spring/service/MessageService.java
+++
@@ -1,8 +0,0 @@
-package me.qinchao.spring.service;
-
-/**
- * Created by SULVTO on 16-3-15.
- */
-public interface MessageService  {
-    String getMessage();
-}
