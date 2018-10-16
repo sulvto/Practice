d518e271dbe5464e5d425248aed3d64e05bc3266
sulvto <sulvto@gmail.com>
2018-10-16 23:13:05 +0800

    docker 中运行php

---
+++ PHP/by-docker/hello.php
@@ -0,0 +1,4 @@
+<?php
+$name = $_REQUEST['name'];
+echo "Hello ".$name;
+?>
---
+++ PHP/by-docker/start-nginx-fpm.sh
@@ -0,0 +1,12 @@
+#
+
+docker stop some-nginx
+docker rm some-nginx
+docker stop php-fpm
+docker rm php-fpm
+docker run --name php-fpm -d -v ${PWD}:/var/www/html:ro php:7.1-fpm
+docker run --name some-nginx -p 8081:80 -d \
+    -v ${PWD}:/usr/share/nginx/html:ro \
+    -v ${PWD}/nginx/conf.d:/etc/nginx/conf.d:ro \
+    --link php-fpm:php \
+    nginx
